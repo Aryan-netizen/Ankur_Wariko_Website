8295d6ecf8ed9a1cf2e98a9fe9a5b76ac7b2c3ac
aryan <gargaryan12122005@gmail.com>
2025-07-22 20:24:27 +0530

    scroll bar problem and adding video in courses section

--- src/main.jsx
+++ src/main.jsx
@@ -4,11 +4,13 @@ import './index.css'
 import App from './App.jsx'
 import { CursorProvider } from './context/CursorContext.jsx'
 import { BrowserRouter } from 'react-router-dom'
+import ScrollToTop from './context/ScrollToTop.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <BrowserRouter>
     <CursorProvider>
+      <ScrollToTop />
       <App />
     </CursorProvider>
     </BrowserRouter>
--- src/pages/Course_Detail.jsx
+++ src/pages/Course_Detail.jsx
@@ -2,14 +2,25 @@ import React from "react";
 
 const Course_Detail = () => {
   return (
-    <div className="h-[200vh] w-full overflow-hidden pt-20 px-10">
+    <div className="h-[250vh] w-full overflow-hidden pt-20 px-10">
       <h1 className="text-5xl font-gilroy-bold">Time Mangement</h1>
-      <h1 className="text-5xl font-gilroy-light mt-2">
+      <h1 className="text-5xl font-gilroy-light mt-4">
         Lorem ipsum dolor sit amet consectetur adipisicing elit. Ratione
         recusandae quaerat quae itaque explicabo? Corporis dolorum praesentium
         earum optio magni, iste voluptate ea ad. Excepturi.
       </h1>
-      
+
+      <div className="w-[80vw] h-[90vh] mx-auto mt-20 relative">
+        <iframe
+          className="absolute top-0 left-0 w-full h-full object-cover opacity-60 transition-all duration-500"
+          src="https://www.youtube.com/embed/7ax4Tb5G1P4?autoplay=1&mute=1&controls=0&rel=0&showinfo=0&modestbranding=1"
+          title=""
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+          referrerPolicy="strict-origin-when-cross-origin"
+          allowFullScreen
+        ></iframe>
+        <div className="w-full h-full bg-transparent absolute top-0 left-0"></div>
+      </div>
       {/* second div */}
       <div className="flex mt-10 gap-4">
         <div className="w-[50%] flex flex-col gap-3 p-2">
@@ -26,6 +37,8 @@ const Course_Detail = () => {
               animi? Mollitia harum voluptatum beatae deleniti perferendis.
             </p>
           </div>
+
+          {/* first div  */}
           <div className="flex flex-col gap-1">
             <h2 className="text-2xl font-gilroy-semi">
               Lorem ipsum dolor sit amet consectetur adipisicing elit. Enim,
@@ -52,7 +65,7 @@ const Course_Detail = () => {
           </div>
         </div>
 
-     {/* second div */}
+        {/* second div */}
         <div className="p-8 w-[50%] h-fit rounded-2xl flex flex-col gap-2">
           <h1 className="text-4xl font-gilroy-bold">What you'll learn</h1>
           <ul className="list-disc mt-3 ml-2">
@@ -94,7 +107,7 @@ const Course_Detail = () => {
             </li>
           </ul>
           <button className="px-8 py-4 mt-8 ml-4 text-xl bg-amber-400 text-black font-gilroy-bold rounded-xl">
-             Enroll Now ➜
+            Enroll Now ➜
           </button>
         </div>
       </div>
